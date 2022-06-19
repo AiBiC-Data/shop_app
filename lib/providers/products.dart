286737680b8715743b8b403d31ffe10e1034886d
AiBiC-Data <whrnjsgh12@naver.com>
2022-06-19 22:25:09 +0900

    modify favorite

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../models/product.dart';
+import './product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
@@ -39,7 +39,7 @@ class Products with ChangeNotifier {
   ];
 
   List<Product> get items {
-    return [...items];
+    return [..._items];
   }
 
   Product findById(String id) {
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: unused_local_variable
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
@@ -14,7 +13,7 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId =
-        ModalRoute.of(context)!.settings.arguments as String; //is the id
+        ModalRoute.of(context)!.settings.arguments as String; // is the id!
     final loadedProduct = Provider.of<Products>(
       context,
       listen: false,
